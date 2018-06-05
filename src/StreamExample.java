81140a9d8b1c6e3a304a3501eec1871f35139ca8
Kwon Young Jae <kyoje11@gmail.com>
2018-06-05 12:44:54 +0900

    Fix Typo

--- src/StreamExample.java
+++ src/StreamExample.java
@@ -52,7 +52,7 @@ public class StreamExample {
         List<Integer> numbers1 = Arrays.asList(1,2,3);
         List<Integer> numbers2 = Arrays.asList(3,4);
         numbers1.stream()
-            .flatMap(i -> number2.stream.map(j->new int[]{i, j}))
+            .flatMap(i -> numbers2.stream().map(j->new int[]{i, j}))
             .collect(Collectors.toList());
 
     }
